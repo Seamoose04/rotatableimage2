0ba1c10f0008c4ca0c1f66d72c0bae3eb86f05b4
Seamus Johnson <72893263+Seamoose04@users.noreply.github.com>
2025-11-07 16:25:29 -0500

    Fixed blocks

--- block-definitions.ts
+++ block-definitions.ts
@@ -3,12 +3,16 @@
 */
 //% color=#0080ff icon="↻"block="Rotatable Image"
 namespace Rotatable {
-    //% block="convert an $img"
+    //% blockId=rotatable_image_create_rotatable block="make $img rotatable"
+    //% blockSetVariable=rotatableImg
+    //% img.shadow = screen_image_picker
     export function createRotatableImage(img: Image): RotatableImage {
         return new RotatableImage(img);
     }
 
     //% block="rotate $rot_img by $degrees"
+    //% rot_img.shadow=variables_get
+    //% rot_img.defl=rotatableImg
     export function rotateImage(rot_img: RotatableImage, degrees: number): void {
         rot_img.rotate(degrees);
     }

ea8d33ee8e40f87f1db1bfebf260518959e92fbd
Seamus Johnson <72893263+Seamoose04@users.noreply.github.com>
2025-11-07 16:49:39 -0500

    Added a get method

--- block-definitions.ts
+++ block-definitions.ts
@@ -13,7 +13,7 @@ namespace Rotatable {
         return new RotatableImage(img);
     }
 
-    //% blockId=rotatable_image_rotate_image block="rotate $rot_img by $degrees"
+    //% blockId=rotatable_image_rotate_image block="rotate $rot_img to $degrees degrees"
     //% rot_img.shadow=variables_get
     //% rot_img.defl=rotatableImg
     //% degrees.defl=0
@@ -21,5 +21,14 @@ namespace Rotatable {
     //% weight=90
     export function rotateImage(rot_img: RotatableImage, degrees: number): void {
         rot_img.rotate(degrees);
+    }
+
+    //% blockId=rotatable_image_get_image block="get image from $rot_img"
+    //% rot_img.shadow=variables_get
+    //% rot_img.defl=rotatableImg
+    //% group="others"
+    //% weight=80
+    export function getImage(rot_img: RotatableImage): Image {
+        return rot_img.getImage()
     }
 }
